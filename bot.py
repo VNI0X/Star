c0c4812dd6f49dbc791f4ac7f9126d966d4afec7
野买 <madhuripatel509@gmail.com>
2025-08-01 16:33:28 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+import plugins.start
+
+app = Client(
+    "StarEntryBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "plugins"}
+)
+
+app.run()
